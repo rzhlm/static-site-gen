4031dcae6a1ff3403e8160251b4e43ca8162f719
rzhlm <118811618+rzhlm@users.noreply.github.com>
2024-07-19 21:35:59 +0200

    corrected mistakes in leafnode

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,13 +1,22 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-		def __init__(self, value: str, tag: str = None, props:dict = None):
-			super().__init__(value: str, tag: str = None, props: dict = None)
-			
-		def to_html(self):
-			if self.value is None:
-				raise ValueError("LeafNode has no value")
-			if self.tag is None:
-				return str(self.value)
-			else:
-				self.props_to_html()
+        def __init__(self, tag, value, props = None):
+            super().__init__(tag, value, children = None, props = props)
+
+        def to_html(self):
+            if self.value is None:
+                raise ValueError("LeafNode has no value")
+            if self.tag is None:
+                return str(self.value)
+            else:
+                self.props_to_html()
+
+        def __eq__(self, other):
+            # super().__eq__(other)
+            
+            if (self.value == other.value
+            and self.tag == other.tag
+            and self.props == other.props):
+                return True
+            return False
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -6,21 +6,21 @@ from leafnode import LeafNode
 
 class TestLeafNode(unittest.TestCase):
     def test_eq(self):
-        node1 = LeafNode("value","tag",{"key": "val"} )
-        node2 = LeafNode("value","tag",{"key": "val"} )
+        node1 = LeafNode(value="value",tag = "tag",props = {"key": "val"})
+        node2 = LeafNode(value="value",tag = "tag",props = {"key": "val"})
         self.assertEqual(node1, node2)
 
-        node_c = LeafNode("value")
-        node_d = LeafNode("value")
+        node_c = LeafNode(None,"value")
+        node_d = LeafNode(None,"value")
         self.assertEqual(node_c, node_d)
 
     def test_not_eq(self):
-        node_a = LeafNode("value","tag",{"key": "val"} )
+        node_a = LeafNode("value","tag",{"key": "val"})
         node_b = LeafNode("value","tag")
         self.assertNotEqual(node_a, node_b)
 
-        node3 = LeafNode("value","tag",{"key": "val"} )
-        node4 = LeafNode("value",{"key": "val"} )
+        node3 = LeafNode("value","tag",{"key": "val"})
+        node4 = LeafNode("value",{"key": "val"})
         self.assertNotEqual(node3, node4)
 	
 
