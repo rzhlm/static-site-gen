3046476a9a89dabb9207ef008f015293dcdbea23
rzhlm <118811618+rzhlm@users.noreply.github.com>
2024-07-20 12:57:18 +0200

    basic functionality of ParentNode-unittest

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -5,16 +5,16 @@ from leafnode import LeafNode
 
 class TestParentNode(unittest.TestCase):
     def test_eq(self):
-        node1 = ParentNode()
-        node2 = ParentNode()
+        node1 = ParentNode([LeafNode("b","node1"), LeafNode("b","node1")])
+        node2 = ParentNode([LeafNode("b","node1"), LeafNode("b","node1")])
         self.assertEqual(node1, node2)
     
-    """
+    
     def test_not_eq(self):
-        node_a = ParentNode()
-        node_b = ParentNode()
+        node_a = ParentNode([LeafNode("b","nodea"), LeafNode("b","nodea")])
+        node_b = ParentNode([LeafNode("b","nodeb"), LeafNode("b","nodeb")])
         self.assertNotEqual(node_a, node_b)
-
+"""
         node3 = ParentNode()
         node4 = ParentNode()
         self.assertNotEqual(node3, node4)
@@ -22,7 +22,7 @@ class TestParentNode(unittest.TestCase):
         node4 = ParentNode()
         node5 = ParentNode()
         self.assertNotEqual(node4, node5)
-        """
+"""    
 
 if __name__ == "__main__":
 	unittest.main()
